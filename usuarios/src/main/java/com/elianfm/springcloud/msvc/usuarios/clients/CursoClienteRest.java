de7cff0c8bfa25e29ef119ffbd4fd602ed244fc6
Elianfm <elianfmujica@gmail.com>
2025-08-25 12:50:22 -0500

    avance hasta la sección 86 - Docker Arguments y Environment Variables

--- usuarios/src/main/java/com/elianfm/springcloud/msvc/usuarios/clients/CursoClienteRest.java
+++ usuarios/src/main/java/com/elianfm/springcloud/msvc/usuarios/clients/CursoClienteRest.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(name = "cursos", url = "http://host.docker.internal:8002")
 // Cuando el Client es también un microservicio, se puede hacer así;
-@FeignClient(name = "cursos", url="http://cursos:8002")
+@FeignClient(name = "cursos", url="${msvc.cursos.url}")
 public interface CursoClienteRest {
 
     @DeleteMapping("/eliminar-curso-usuario/{id}")
